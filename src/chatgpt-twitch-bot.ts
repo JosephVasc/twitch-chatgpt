ca853f80598409f55614bb1e5fe30f30d91f05f4
JosephVasc <71454189+JosephVasc@users.noreply.github.com>
2023-01-07 21:42:52 -0500

    second commit

---
+++ src/chatgpt-twitch-bot.ts
@@ -0,0 +1,57 @@
+import { ChatGPTAPI, getOpenAIAuth } from "chatgpt";
+import tmi from "tmi.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+//implement text to speech
+function textToSpeech(text: string) {
+  const synth = window.speechSynthesis;
+  const utterThis = new SpeechSynthesisUtterance(text);
+  synth.speak(utterThis);
+}
+
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  identity: {
+    username: process.env.TWITCH_NAME,
+    password: process.env.TWITCH_TOKEN,
+  },
+  channels: [process.env.TWITCH_CHANNEL as string],
+});
+
+let chatGPTAPI: ChatGPTAPI;
+
+(async () => {
+  const openAIAuth = await getOpenAIAuth({
+    email: process.env.OPENAI_EMAIL,
+    password: process.env.OPENAI_PASSWORD,
+  });
+
+  chatGPTAPI = new ChatGPTAPI({ ...openAIAuth });
+  await chatGPTAPI.ensureAuth();
+})();
+
+client.connect();
+
+client.on("message", async (channel, tags, message, self) => {
+  if (self || !message.startsWith("!")) {
+    return;
+  }
+
+  const args = message.slice(1).split(" ");
+  const command = args.shift()?.toLowerCase();
+
+  if (command === "chatgpt test") {
+    client.say(channel, `@${tags.username}, test`);
+  } else if (command === "chatgpt") {
+    const prompt = args.join(" ");
+
+    const response = await chatGPTAPI.sendMessage(prompt);
+
+    client.say(channel, `@${tags.username}, ${response}`);
+  }
+});
